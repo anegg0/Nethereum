ca5e0d113af2876e992815aa82630946416896d7
anegg0 <gael.offers@protonmail.com>
2019-06-20 15:15:55 -0400

    juan fix retrieve live hash

--- src/Nethereum.Pantheon.IntegrationTests/TestSettings.cs
+++ src/Nethereum.Pantheon.IntegrationTests/TestSettings.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Linq;
 using Microsoft.Extensions.Configuration;
-//using System.Configuration;
 
 namespace Nethereum.RPC.Tests.Testers
 {
--- src/Nethereum.Pantheon.IntegrationTests/Testers/IbftGetValidatorsByBlockHashTester.cs
+++ src/Nethereum.Pantheon.IntegrationTests/Testers/IbftGetValidatorsByBlockHashTester.cs
@@ -8,6 +8,7 @@ using Nethereum.Pantheon.IntegrationTests;
 using Nethereum.RPC.Tests.Testers;
 using Newtonsoft.Json.Linq;
 using Xunit;
+using Nethereum.Hex.HexTypes;
 
 namespace Nethereum.Pantheon.Tests.Testers
 {
@@ -16,8 +17,11 @@ namespace Nethereum.Pantheon.Tests.Testers
     {
         public override async Task<string[]> ExecuteAsync(IClient client)
         {
+            var web3 = new Web3.Web3(client);
+            var block = await  web3.Eth.Blocks.GetBlockWithTransactionsByNumber.SendRequestAsync(new HexBigInteger(1));
+            var blockHash = block.BlockHash;
             var ibftGetValidatorsByBlockHash = new IbftGetValidatorsByBlockHash(client);
-            return await ibftGetValidatorsByBlockHash.SendRequestAsync(Settings.GetBlockHash());
+            return await ibftGetValidatorsByBlockHash.SendRequestAsync(blockHash);
         }
 
         public override Type GetRequestType()
